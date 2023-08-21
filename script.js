8615d2b91a2568ab26fc6d086fa53174badc62a1
Md Sarfraj <116196482+mdsarfarj@users.noreply.github.com>
2023-08-21 23:23:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+
+for (let i = 0; i < 5; i++) {
+  const randomTime = Math.floor(Math.random() * 5) + 1;
+  promises.push(new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(randomTime);
+    }, randomTime * 1000);
+  }));
+}
+
+Promise.any(promises)
+  .then((result) => {
+    const outputDiv = document.getElementById("output");
+    outputDiv.textContent = `Result: ${result}`;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
